3a750e126b5508ccbea92dee77cf2741416f8af1
bhivam <shivamkajaria@gmail.com>
2022-12-28 17:12:30 -0500

    Implemented swipe to change page funcationality

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,15 +1,14 @@
-import 'package:hackru/weather/utils/weather_type.dart';
-import 'package:hackru/weather/bg/weather_bg.dart';
+import 'package:hackru/defaults.dart';
 import 'package:hackru/models/cred_manager.dart';
 import 'package:hackru/styles.dart';
 import 'package:hackru/ui/pages/annoucements/announcements.dart';
 import 'package:hackru/ui/pages/dashboard/dashboard.dart';
 import 'package:hackru/ui/pages/events/events.dart';
+import 'package:hackru/ui/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-
-import 'help/help.dart';
-import 'login/login_page.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class Home extends StatefulWidget {
   static const String routeName = '/material/bottom_navigation';
@@ -18,128 +17,199 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool _hasAuthToken = false;
   int _currentBottomNavItemIndex = 1;
-  bool showHelp = false;
-  bool showLogin = false;
+  CredManager? credManager;
 
-  final PageController _pageController = PageController(initialPage: 1);
+  @override
+  void initState() {
+    credManager = Provider.of<CredManager>(context, listen: false);
+    super.initState();
+    _hasToken();
+  }
 
-  void setHelp(bool val) => setState(() => showHelp = val);
-  void setLogin(bool val) => setState(() => showLogin = val);
+  void _hasToken() async {
+    var hasToken = credManager!.hasCredentials();
+    if (hasToken) {
+      setState(() {
+        _hasAuthToken = hasToken;
+      });
+    } else {
+      setState(() {
+        _hasAuthToken = false;
+      });
+    }
+  }
 
-  final _bottomNavNames = [
-    "Announcements",
-    "DashBoard",
-    "Events",
-  ];
-  final _bottomNavIcons = [
-    Icons.announcement,
-    Icons.dashboard,
-    Icons.calendar_month,
-  ];
+  PageController _pageController = PageController(initialPage: 1);
 
+  ///===========================================================
+  ///                     BOTTOM NAV PAGES
+  ///===========================================================
+  final _kBottomNavPages = <Widget>[Announcements(), Dashboard(), Events()];
+
+  ///===========================================================
+  ///                      BOTTOM APP BAR
+  ///===========================================================
   BottomAppBar _buildBottomAppBar(BuildContext context) {
     return BottomAppBar(
-      shape: const CircularNotchedRectangle(),
+      shape: CircularNotchedRectangle(),
       elevation: 25.0,
       notchMargin: 6.0,
-      color: Colors.transparent,
+      color: Theme.of(context).primaryColor,
       child: Row(
-          children: [0, 1, 2]
-              .map((idx) => bottNavBarIcon(
-                  idx, _bottomNavNames[idx], _bottomNavIcons[idx]))
-              .toList()),
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: InkResponse(
+              onTap: () {
+                setState(() {
+                  _currentBottomNavItemIndex = 0;
+                });
+                _pageController.animateToPage(0,
+                    duration: Duration(milliseconds: 250),
+                    curve: Curves.easeIn);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.announcement,
+                      size: 25.0,
+                      color: _currentBottomNavItemIndex == 0
+                          ? HackRUColors.white
+                          : HackRUColors.charcoal_dark,
+                    ),
+                    Text(
+                      'Announcements',
+                      style: TextStyle(
+                        fontSize: _currentBottomNavItemIndex == 0 ? 14.0 : 12.0,
+                        color: _currentBottomNavItemIndex == 0
+                            ? HackRUColors.white
+                            : HackRUColors.charcoal_dark,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: InkResponse(
+              onTap: () {
+                setState(() {
+                  _currentBottomNavItemIndex = 1;
+                });
+                _pageController.animateToPage(1,
+                    duration: Duration(milliseconds: 250),
+                    curve: Curves.easeIn);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.home,
+                      size: 25.0,
+                      color: _currentBottomNavItemIndex == 1
+                          ? HackRUColors.white
+                          : HackRUColors.charcoal_dark,
+                    ),
+                    Text(
+                      'Dashboard',
+                      style: TextStyle(
+                        fontSize: _currentBottomNavItemIndex == 1 ? 14.0 : 12.0,
+                        color: _currentBottomNavItemIndex == 1
+                            ? HackRUColors.white
+                            : HackRUColors.charcoal_dark,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: InkResponse(
+              onTap: () {
+                setState(() {
+                  _currentBottomNavItemIndex = 2;
+                });
+                _pageController.animateToPage(2,
+                    duration: Duration(milliseconds: 250),
+                    curve: Curves.easeIn);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      FontAwesomeIcons.calendarAlt,
+                      size: 25.0,
+                      color: _currentBottomNavItemIndex == 2
+                          ? HackRUColors.white
+                          : HackRUColors.charcoal_dark,
+                    ),
+                    Text(
+                      'Events',
+                      style: TextStyle(
+                        fontSize: _currentBottomNavItemIndex == 2 ? 14.0 : 12.0,
+                        color: _currentBottomNavItemIndex == 2
+                            ? HackRUColors.white
+                            : HackRUColors.charcoal_dark,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
+  ///===========================================================
+  ///                     BUILD FUNCTION
+  ///===========================================================
+
   @override
   Widget build(BuildContext context) {
-    final _kBottomNavPages = <Widget>[
-      Announcements(),
-      Dashboard(
-        goToHelp: () => setHelp(true),
-        goToLogin: () => setLogin(true),
-      ),
-      Events(),
-    ];
-
-    return Stack(children: [
-      WeatherBg(
-          weatherType: WeatherType.sunnyNight,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height),
-      showHelp
-          ? Help(
-              () => setHelp(false),
-              HackRUColors.transparent,
-              HackRUColors.off_white_blue,
-              Colors.black26,
-              HackRUColors.blue_grey)
-          : Container(),
-      showLogin
-          ? Provider.value(
-              value: Provider.of<CredManager>(context),
-              child: LoginPage(goToDashboard: () => setLogin(false)))
-          : Container(),
-      !(showLogin || showHelp)
-          ? Scaffold(
-              backgroundColor: Colors.transparent,
-              body: Padding(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).padding.top * 1.5),
-                child: PageView(
-                  controller: _pageController,
-                  children: _kBottomNavPages,
-                  onPageChanged: (value) => setState(() {
-                    _currentBottomNavItemIndex = value;
-                  }),
-                ),
-              ),
-              bottomNavigationBar: _buildBottomAppBar(context),
-            )
-          : Container()
-    ]);
-  }
-
-  Widget bottNavBarIcon(int index, String name, IconData icon) {
-    return Expanded(
-      flex: 2,
-      child: InkResponse(
-        onTap: () {
-          setState(() {
-            _currentBottomNavItemIndex = index;
-          });
-          _pageController.animateToPage(index,
-              duration: const Duration(milliseconds: 250),
-              curve: Curves.easeIn);
-        },
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 5.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                icon,
-                size: 25.0,
-                color: _currentBottomNavItemIndex == index
-                    ? HackRUColors.white
-                    : HackRUColors.charcoal_dark,
-              ),
-              Text(
-                name,
-                style: TextStyle(
-                  fontSize: _currentBottomNavItemIndex == index ? 14.0 : 12.0,
-                  color: _currentBottomNavItemIndex == index
-                      ? HackRUColors.white
-                      : HackRUColors.charcoal_dark,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ],
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        left: false,
+        right: false,
+        bottom: false,
+        child: Scaffold(
+          key: _scaffoldKey,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          // body: _kBottomNavPages[_currentBottomNavItemIndex],
+          body: PageView(
+            controller: _pageController,
+            children: _kBottomNavPages,
+            onPageChanged: (value) => setState(() {
+              _currentBottomNavItemIndex = value;
+            }),
           ),
+          bottomNavigationBar: _buildBottomAppBar(context),
         ),
       ),
     );
